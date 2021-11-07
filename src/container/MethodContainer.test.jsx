7498a8d2b016f585fa9cd1a06d36c37644d3e70a
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-11-07 14:24:55 -0800

    behavioural test for container failing, cannot fine label of url

--- src/container/MethodContainer.test.jsx
+++ src/container/MethodContainer.test.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import MethodContainer from './MethodContainer';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 describe('should render entire content of container', () => {
-    it('renders each presentational component within the method container', () => {
+    it('renders each presentational component within the method container', async () => {
         render(
             <MethodContainer />
         );
         expect(MethodContainer).toMatchSnapshot();
+
+        // const urlInput = await screen.findAllByLabelText('url');
+        // userEvent.type(urlInput, 'https://jsonplaceholder.typicode.com/posts/1')
+
+        // const methodInput = await screen.findAllByLabelText('GET');
+        // userEvent.click(methodInput, 'GET');
+
+        // const submitButton = await screen.findByRole('button', {
+        //     name: 'make-request'
+        // });
+
+        // userEvent.click(submitButton);
+
+        // return waitFor(() => {
+        //     const content = screen.findAllByLabelText('userId', {
+        //         exact: false
+        //     });
+
+        //     expect(content).toHaveLength(1);
+        // });
     });
 });
